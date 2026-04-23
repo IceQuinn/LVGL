738f8f2cfda482304d9d2a639d7fd95f09d66e9c
HomeQ <1372197809@qq.com>
2026-04-23 23:20:36 +0800

    1、增加按键驱动，已完成软件包按键功能验证

--- LVGL/applications/main.c
+++ LVGL/applications/main.c
@@ -14,8 +14,28 @@
 #define DBG_LVL DBG_LOG
 #include <rtdbg.h>
 
+#include <drv_common.h>
+#include "agile_button.h"
+
+static agile_btn_t _sbtn;
+
+//{GET_PIN(B, 1), KEY_EVENT_NONE, "返回键"},
+//{GET_PIN(B, 0), KEY_EVENT_NONE, "上键"},
+//{GET_PIN(A, 1), KEY_EVENT_NONE, "下键"},
+//{GET_PIN(A, 0), KEY_EVENT_NONE, "确定键"},
+
+static void btn_click_event_cb(agile_btn_t *btn)
+{
+    rt_kprintf("[button click event] pin:%d   repeat:%d, hold_time:%d\r\n", btn->pin, btn->repeat_cnt, btn->hold_time);
+}
 int main(void)
 {
+//    agile_btn_env_init();
+
+    agile_btn_stop(&_sbtn);
+    agile_btn_init(&_sbtn, GET_PIN(B, 1), PIN_LOW, PIN_MODE_INPUT_PULLUP);
+    agile_btn_set_event_cb(&_sbtn, BTN_CLICK_EVENT, btn_click_event_cb);
+    agile_btn_start(&_sbtn);
 //    int count = 1;
 //
 //    while (count++)
