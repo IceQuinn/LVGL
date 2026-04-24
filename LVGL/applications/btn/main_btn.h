f59d8d0e637eb1423421fcaac44701022594de81
IceQuinn <1372197809@qq.com>
2026-04-24 15:34:33 +0800

    1、完成按键驱动适配

---
+++ LVGL/applications/btn/main_btn.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2006-2021, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2026-04-24     mutou       the first version
+ */
+#ifndef APPLICATIONS_BTN_MAIN_BTN_H_
+#define APPLICATIONS_BTN_MAIN_BTN_H_
+
+void Main_Btn_Init(void);
+
+#endif /* APPLICATIONS_BTN_MAIN_BTN_H_ */
